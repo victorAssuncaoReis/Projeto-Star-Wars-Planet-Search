3b004e98fa8950b8a281454c9ad0e138f5651e2c
Victor <victorreis222@gmail.com>
2022-11-23 15:38:38 -0300

    Parte do requisito 03 - Inputs e states

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,6 +4,9 @@ import '../App.css';
 function Table() {
   const [planets, setPlanets] = useState([]);
   const [inputFilter, setInputFilter] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState('0');
 
   const apiFetch = async () => {
     fetch('https://swapi.dev/api/planets')
@@ -23,6 +26,41 @@ function Table() {
         data-testid="name-filter"
         onChange={ (e) => setInputFilter(e.target.value) }
       />
+      <select
+        name="column-filter"
+        data-testid="column-filter"
+        onChange={ (e) => setColumnFilter(e.target.value) }
+        value={ columnFilter }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        name="comparison-filter"
+        id="comparison-filter"
+        data-testid="comparison-filter"
+        onChange={ (e) => setComparisonFilter(e.target.value) }
+        value={ comparisonFilter }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        onChange={ (e) => setValueFilter(e.target.value) }
+        value={ valueFilter }
+      />
+      <button
+        data-testid="button-filter"
+        type="button"
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <tr>
